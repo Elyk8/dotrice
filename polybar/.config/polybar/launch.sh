2bb30da80018fb22cbea37409c3e5b5b0f8a8244
elyk8 <kylese58@gmail.com>
2022-08-12 09:03:57 +1000

    herbsluftwm

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
+
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar i3monitor1 2>&1 | tee -a /tmp/polybar1.log &
-disown
-
-if [ "$(uname -n)" = "skynet" ]; then
-	polybar i3monitor2 2>&1 | tee -a /tmp/polybar2.log &
-	disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mainbar-herbst &
+  done
+else
+  polybar --reload mainbar-herbst &
 fi
 
 echo "Bars launched..."
