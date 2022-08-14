9b42a5ab6987cdae112f95c264c0f5a61ebd6a09
elyk8 <kylese58@gmail.com>
2022-08-14 23:30:54 +1000

    remove and add

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,17 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-# Terminate already running bar instances
-polybar-msg cmd quit
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-herbst &
-  done
-else
-  polybar --reload mainbar-herbst &
+if ps -C herbstluftwm >/dev/null 2>&1; then
+  LAUNCH="herbst-bar"
+elif ps -C i3 >/dev/null 2>&1; then
+  LAUNCH="i3-bar"
 fi
 
-echo "Bars launched..."
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [ "$(xrandr --listactivemonitors | awk '/Monitors/ {print $2}' )" = 1 ]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) polybar "${LAUNCH}1" &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [ "$primary" = "$m" ]; then
+        MONITOR=$m TRAY_POS=right polybar "${LAUNCH}1" &
+    else
+        MONITOR=$m TRAY_POS=none polybar "${LAUNCH}2" &
+    fi
+  done
+fi
